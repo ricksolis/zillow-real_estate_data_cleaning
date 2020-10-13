1c8f3c083258a0dc1f57ea3f45b6e86f20bf0c9b
Rick Solis <rick.solis@gmail.com>
2020-10-12 22:39:33 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Cleaning up the Zillow Real Estate Data for Analysis',
+    author='Rick Solis',
+    license='',
+)
